673c61463788d37fffd1daa1e6348ffdc8825555
55555-Jyeon <jyeon380516@gmail.com>
2024-03-25 15:07:01 +0900

    change id-card to image

--- src/app/Amy/AboutMe/_component/SideBar.tsx
+++ src/app/Amy/AboutMe/_component/SideBar.tsx
@@ -5,31 +5,14 @@ import { url } from "inspector";
 
 export default function SideBar() {
   return (
-    <div>
+    <div className={style.imageContainer}>
       <Image
-        className={style.strap}
-        width={200}
-        height={300}
-        src="/images/strap.png"
-        alt="strap"
+        className={style.image}
+        width={500}
+        height={700}
+        src="/images/IDcard.svg"
+        alt="id card"
       />
-      <div className={style.glassEffect} />
-      <div className={style.container}>
-        <h1>WEB | FE DEVELOPER</h1>
-        <div className={style.imageContainer}>
-          <Image
-            width={200}
-            height={300}
-            src="/images/menu05.jpg"
-            alt="profile image"
-          />
-        </div>
-        <div className={style.textbox}>
-          <h1>JUYEON OH</h1>
-          <h1>오&nbsp;주&nbsp;연</h1>
-          <h1></h1>
-        </div>
-      </div>
       <div className={style.iconbox}>
         <Link href="https://velog.io/@55555-jyeon/posts">
           <div
